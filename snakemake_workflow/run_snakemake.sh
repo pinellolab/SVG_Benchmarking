e4b988c0fe1e40ef078a173d687778e9ffb64049
lzj1769 <lzj1769@gmail.com>
2024-07-05 09:52:20 -0400

    clean up

--- snakemake_workflow/run_snakemake.sh
+++
@@ -1,8 +0,0 @@
-#snakemake --snakefile ./00_simulation_data.smk -k --nolock --use-conda --cores 100 --conda-base-path /data/pinello/SHARED_SOFTWARE/anaconda_latest
-#snakemake --snakefile ./01_scalability.smk -k --use-conda --cores 10 --conda-base-path /data/pinello/SHARED_SOFTWARE/anaconda_latest
-#snakemake --snakefile ./02_shuffle_data.smk -k --nolock --use-conda --cores 10 --conda-base-path /data/pinello/SHARED_SOFTWARE/anaconda_latest
-#snakemake --snakefile ./03_human_heart.smk -k --use-conda --cores 10 --conda-base-path /data/pinello/SHARED_SOFTWARE/anaconda_latest
-#snakemake --snakefile ./04_clustering.smk -k --nolock --use-conda --cores 10 --conda-base-path /data/pinello/SHARED_SOFTWARE/anaconda_latest
-#snakemake --snakefile ./05_real_data_shuffle.smk -k --nolock --use-conda --cores 50 --conda-base-path /data/pinello/SHARED_SOFTWARE/anaconda_latest
-snakemake --snakefile ./06_spatial_atac.smk -k --nolock --use-conda --cores 1 --conda-base-path /data/pinello/SHARED_SOFTWARE/anaconda_latest
-#snakemake --snakefile ./07_cell_type_deconvolution.smk -k --nolock --use-conda --cores 50 --conda-base-path /data/pinello/SHARED_SOFTWARE/anaconda_latest
--- snakemake_workflow/scripts/utils/scGCO_simple/__init__.py
+++
@@ -1,5 +0,0 @@
-from .Preprocessing import *
-from .Graph_cut import *
-from .Visualization import *
-from .GMM import *
-__version__ = '1.1.2'
