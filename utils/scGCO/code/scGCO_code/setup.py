0798069fe53cd0a1493b288d9e9bf05f5d81c2bc
lzj1769 <lzj1769@gmail.com>
2026-01-09 11:55:58 -0500

    add utils

---
+++ utils/scGCO/code/scGCO_code/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+        name='scGCO',
+        version='1.1.0',
+        description='single-cell graph cuts optimization',
+        url='https://github.com/WangPeng-Lab/scGCO',
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=['pandas','numpy','matplotlib','scipy','scikit-learn','seaborn','parmap','Cython','pygco',
+                        'tqdm','networkx','shapely','statsmodels','hdbscan','pillow'],
+        author='Peng Wang',
+        author_email='wangpeng@picb.ac.cn',
+        license='MIT'
+
+)
+
+## pip install numpy
+## pip install pygco
+## conda install -c conda-forge cython
+## pip uninstall shapely && conda install -c r shapely   
